559145a9ac249322bafb871fd1c4b55f63807e73
nazmussaadatcse <nazmus.bd97@gmail.com>
2023-08-17 19:18:36 +0600

    server

---
+++ _deploySteps.js
@@ -0,0 +1,6 @@
+/**
+ * add vercel config
+ * vercel
+ * add "build": "node index.js" in package.json
+ * 
+ */
---
+++ index.js
@@ -0,0 +1,174 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const app = express()
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+// middle wares 
+app.use(cors());
+app.use(express.json())
+
+
+// console.log(process.env.DB_USER)
+// console.log(process.env.DB_PASSWORD)
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@smartcarservices.gwnkybs.mongodb.net/?retryWrites=true&w=majority`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+function verifyJWT(req,res, next){
+    console.log('verifyJWT:', req.headers.authorization);
+
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message:'unauthorized access'});
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,function(err, decoded){
+        if(err){
+            console.log('err:',err);
+
+            return res.status(401).send({message:'unauthorized access'});
+        }
+        req.decoded = decoded;
+        console.log('decoded:',decoded);
+        next();
+    })
+}
+
+
+async function run() {
+  try {
+    const serviceCollection = client.db('smartCar').collection('services');
+    const orderCollection = client.db('smartCar').collection('orders');
+
+
+    app.post('/jwt',(req,res)=>{
+        const user = req.body;
+        console.log(user);
+        const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1d'})
+        res.send({token})
+    })
+
+    app.get('/services',async(req,res)=>{
+        const search = req.query.search;
+        console.log(search)
+        let query = {};
+        if(search.length){
+            query = {
+                $text:{
+                    $search: search
+                }
+            }
+        }
+        
+        
+        // const query = {price:{$gt:100, $lt:300}}
+        const order = req.query.order === 'asc'? 1 : -1;
+        console.log(query,'search?')
+
+        const cursor = serviceCollection.find(query).sort({price:order});
+        console.log(query,'search')
+
+        const services = await cursor.toArray();
+        console.log(query,'search//')
+
+        res.send(services);
+        console.log(query,'search....')
+
+    });
+
+    app.get('/services/:id', async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const service = await serviceCollection.findOne(query);
+        res.send(service);
+
+    });
+
+    // orders api
+
+    app.get('/orders', verifyJWT, async(req,res)=>{
+        const decoded = req.decoded;
+        // console.log(req.headers.authorization)
+        console.log('inside orders api',decoded)
+
+        if(decoded.email!==req.query.email){
+            res.status(403).send({message:'unauthorized access'})
+        }
+        let query = {};
+
+        if(req.query.email){
+            query = {
+                email: req.query.email
+            }
+        }
+        const cursor = orderCollection.find(query);
+        const orders = await cursor.toArray();
+        res.send(orders);
+
+    });
+
+
+    app.post('/orders', async(req,res)=>{
+        const order = req.body;
+        const result = await orderCollection.insertOne(order);
+        res.send(result);
+
+    });
+
+
+
+    app.patch('/orders/:id',async(req,res)=>{
+        const id = req.params.id;
+        const status = req.body.status;
+        const query = {_id: new ObjectId(id)}
+        const updatedDoc = {
+            $set:{
+                status: status
+            }
+        }
+        const result = await orderCollection.updateOne(query, updatedDoc);
+        res.send(result);
+    });
+
+
+
+    app.delete('/orders/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await orderCollection.deleteOne(query);
+        res.send(result);
+    })
+
+  } finally {
+    
+  }
+}
+run().catch(err=> console.error(err));
+
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send('>> smart car server running');
+    
+})
+
+app.listen(port,()=>{
+    console.log(`>> smart car server running on port ${port}`);
+})
